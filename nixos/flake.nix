d862a5892c4c3e4b218c90420548413fa104ea43
NikSne <niksne.mc@yandex.ru>
2024-02-26 22:05:17 +0300

    update rust

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -5,9 +5,11 @@
       nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
       nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.11";
+
+      rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { nixpkgs, nixpkgs-stable, ... } @ inputs:
+  outputs = { nixpkgs, nixpkgs-stable, rust-overlay, ... } @ inputs:
   {
     nixosConfigurations = {
       laptop-niksne = nixpkgs.lib.nixosSystem rec {
--- nixos/modules/packages/development/rust.nix
+++ nixos/modules/packages/development/rust.nix
@@ -1,11 +1,31 @@
-{ pkgs, ... }:
+{ inputs, pkgs, ... }:
 
 {
+  # Apply the overlay to the package set
+  nixpkgs.overlays = [
+    inputs.rust-overlay.overlays.default
+  ];
 
   environment.systemPackages = with pkgs; [
-    rustup
-    cargo
-    rustc
-    pkg-config
+    (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
+    lldb_16
+    taplo #toml formatter & lsp
+    cargo-watch
+    cargo-deny
+    cargo-audit
+    cargo-update
+    cargo-edit
+    cargo-outdated
+    cargo-license
+    cargo-tarpaulin
+    cargo-cross
+    cargo-zigbuild
+    cargo-nextest
+    cargo-spellcheck
+    cargo-modules
+    cargo-bloat
+    cargo-unused-features
+    bacon
+    evcxr #rust repl
   ];
 }

c1acb2ea0f7b5f410268fe117cd23710982646a6
NikSne <niksne.mc@yandex.ru>
2024-02-26 22:12:17 +0300

    Update rust.nix

--- nixos/modules/packages/development/rust.nix
+++ nixos/modules/packages/development/rust.nix
@@ -8,24 +8,5 @@
 
   environment.systemPackages = with pkgs; [
     (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
-    lldb_16
-    taplo #toml formatter & lsp
-    cargo-watch
-    cargo-deny
-    cargo-audit
-    cargo-update
-    cargo-edit
-    cargo-outdated
-    cargo-license
-    cargo-tarpaulin
-    cargo-cross
-    cargo-zigbuild
-    cargo-nextest
-    cargo-spellcheck
-    cargo-modules
-    cargo-bloat
-#    cargo-unused-features
-    bacon
-    evcxr #rust repl
   ];
 }

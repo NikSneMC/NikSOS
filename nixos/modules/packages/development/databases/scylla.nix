80ef518259ac4f33293740661140acabe140757c
NikSne <niksne.mc@yandex.ru>
2024-02-26 19:52:02 +0300

    Update scylla.nix

--- nixos/modules/packages/development/databases/scylla.nix
+++ nixos/modules/packages/development/databases/scylla.nix
@@ -1,11 +1,11 @@
-{ fetchFromGitHub, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   nixpkgs.overlays = [
     (final: prev: {
       scylladb = prev.scylladb.overrideAttrs(old: rec {
         version = "5.2.15";
-        src = fetchFromGitHub {
+        src = prev.fetchFromGitHub {
             owner = "scylladb";
             repo = "scylladb";
             rev = "fd32e2ee103acaa54cc7362b64eccd6a7f6aaf38";

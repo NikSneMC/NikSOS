f9a2b5067df13746d8ed848ea6acc6f196e84401
NikSne <niksne.mc@yandex.ru>
2024-02-26 20:26:50 +0300

    Update apps_fix.sh

--- hypr/scripts/apps_fix.sh
+++ hypr/scripts/apps_fix.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 sleep 1
-killall -e xdg-desktop-portal-hyprland
-killall -e xdg-desktop-portal-wlr
-killall xdg-desktop-portal
-/usr/lib/xdg-desktop-portal-hyprland &
+pkill -9 -e xdg-desktop-portal-hyprland
+pkill -9 -e xdg-desktop-portal-wlr
+pkill -9 xdg-desktop-portal
+/run/current-system/sw/libexec/xdg-desktop-portal-hyprland
 sleep 2
-/usr/lib/xdg-desktop-portal &
+/run/current-system/sw/libexec/xdg-desktop-portal & pkill -9 pipewire && pkill -9 pipewire-pulse

546c3cb3a3c22af58a21609ea44edc01233fabad
NikSne <niksne.mc@yandex.ru>
2024-03-02 18:18:40 +0300

    fix themes

--- fish/functions/autostart.fish
+++ fish/functions/autostart.fish
@@ -1,7 +1,6 @@
 function autostart
     pipewire & \
     swaync -c ~/.config/swaync/config.json & \
-    ~/.config/hypr/scripts/apps_fix.sh & \
     dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP & \
     systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP & \
     waybar & \
@@ -10,6 +9,8 @@ function autostart
     swww init && swww img ~/Pictures/Wallpapers/catppuccin-mocha-teal.jpg & \
     wl-paste --type text --watch cliphist store & \
     wl-paste --type image --watch cliphist store & \
+    hyprctl setcursor Catppuccin-Mocha-Teal 24 &\
+    ~/.config/hypr/scripts/apps_fix.sh & \
     swayidle -w & \
     vesktop & \
     telegram-desktop & \

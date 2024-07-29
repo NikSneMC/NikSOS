6ba312fa569d62f83e2a097a92b3ac25342ebe16
NikSne <commits@niksne.ru>
2024-07-29 17:26:01 +0300

    programs/browsers: update floorp

--- home/niksne/programs/browsers/floorp.nix
+++ home/niksne/programs/browsers/floorp.nix
@@ -5,5 +5,12 @@
   programs.firefox = {
     enable = true;
     package = pkgs.floorp;
+    nativeMessagingHosts = with pkgs; [
+      firefoxpwa
+    ];
   };
+
+  home.packages = with pkgs; [
+    firefoxpwa
+  ];
 }

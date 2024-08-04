99cee7eaf91254307cae97af8783da1ca94530d9
NikSne <commits@niksne.ru>
2024-08-04 03:28:49 +0300

    home/niksne/programs/browsers/vivaldi: fix proxy

--- home/niksne/programs/browsers/vivaldi.nix
+++ home/niksne/programs/browsers/vivaldi.nix
@@ -12,7 +12,6 @@
     commandLineArgs = [
       "--enable-features=UseOzonePlatform"
       "--ozone-platform=wayland"
-      "--proxy-server="http://127.0.0.1:8888""
     ];
     extensions = [
       "kjhdffcfinhkdfbbhjlfoadcdfgihmlp"
@@ -20,6 +19,7 @@
       "apmmpaebfobifelkijhaljbmpcgbjbdo"
       "cbghhgpcnddeihccjmnadmkaejncjndb"
       "mnjggcdmjocbbbhaepdhchncahnbgone"
+      "onnfghpihccifgojkpnnncpagjcdbjod"
     ];
   };
 

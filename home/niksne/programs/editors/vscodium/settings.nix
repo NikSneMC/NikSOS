a75d584ada6ff937a1bf61d4cde15a0455116312
NikSne <commits@niksne.ru>
2024-08-05 19:56:46 +0300

    feat: sync vscodium theme with user theme

--- home/niksne/programs/editors/vscodium/settings.nix
+++ home/niksne/programs/editors/vscodium/settings.nix
@@ -1,12 +1,23 @@
 {
+  config,
+  lib,
+  ...
+}: let
+  mkUpper =
+    str:
+    (lib.toUpper (builtins.substring 0 1 str)) + (builtins.substring 1 (builtins.stringLength str) str);
+in {
   programs.vscode.userSettings = {
-    workbench.colorTheme = "Catppuccin Mocha";
-    catppuccin.accentColor = "sky";
+    workbench.colorTheme = "Catppuccin ${mkUpper config.theme.flavor}";
+    catppuccin.accentColor = config.theme.accent;
     vscord.app.name = "VSCodium";
     files.associations = {
       "*.lock" = "json";
     };
-    editor.fontLigatures = true;
+    editor = {
+      fontFamily = "'JetBrainsMono Nerd Font', 'monospace', monospace";
+      fontLigatures = true;
+    };
     codeium.enableConfig = {
       "*" = true;
       nix = true;

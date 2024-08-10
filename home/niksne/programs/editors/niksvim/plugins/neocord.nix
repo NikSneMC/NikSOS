4c1cd2851e6bc2dec4a744f581681fa895c377cf
NikSne <commits@niksne.ru>
2024-08-10 14:15:50 +0300

    fix (neocord): rename: nixvim -> niksvim

--- home/niksne/programs/editors/niksvim/plugins/neocord.nix
+++ home/niksne/programs/editors/niksvim/plugins/neocord.nix
@@ -4,7 +4,7 @@
     settings = {
       client_id = "1251330515147948083";
       logo = "https://cdn.discordapp.com/app-icons/1251330515147948083/d338503f6533c73291cc10719c80c623.png";
-      logo_tooltip = "NixVim";
+      logo_tooltip = "NikSVim";
     };  
   };
 }

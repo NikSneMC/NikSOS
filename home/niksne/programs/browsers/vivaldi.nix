5743e3fb4e9b771b317e72dd509d41038a0abf8b
NikSne <commits@niksne.ru>
2024-08-22 14:49:01 +0300

    fix (vivaldi): change proxy extension

--- home/niksne/programs/browsers/vivaldi.nix
+++ home/niksne/programs/browsers/vivaldi.nix
@@ -19,7 +19,7 @@
       "apmmpaebfobifelkijhaljbmpcgbjbdo"
       "cbghhgpcnddeihccjmnadmkaejncjndb"
       "mnjggcdmjocbbbhaepdhchncahnbgone"
-      "onnfghpihccifgojkpnnncpagjcdbjod"
+      "pljikjgdcelenhecaainhheabilopmcc"
     ];
   };
 

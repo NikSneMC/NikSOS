c43179c23c894048266d624e831c4936181af7fc
NikSne <commits@niksne.ru>
2024-08-26 14:34:11 +0300

    feat (niksvim): unmap arrow keys

--- home/niksne/programs/editors/niksvim/binds.nix
+++ home/niksne/programs/editors/niksvim/binds.nix
@@ -5,17 +5,23 @@
   mkKeymaps = keymaps: lib.mapAttrsToList (
     name: value: let 
       keymap = {
-        key = name;
-        action = value;
-        options.silent = true;
-      };
-    in if builtins.isString value then keymap else keymap // value
+        key = name; 
+        action = value; 
+        options.silent = true; 
+      }; 
+    in (if builtins.isString value then keymap else keymap // value)
   ) keymaps;
+  mkDisabledKeymaps = keymaps: mkKeymaps (builtins.listToAttrs (builtins.map (key: lib.nameValuePair key "") keymaps));
 in {
-  programs.nixvim.keymaps = mkKeymaps {
+  programs.nixvim.keymaps = (mkKeymaps {
     "jk" = {
-      action ="<Esc>";
+      action = "<Esc>";
       mode = [ "i" "v" ];
     };
-  };
+  }) ++ (mkDisabledKeymaps [
+    "<Up>"
+    "<Down>"
+    "<Left>"
+    "<Right>"
+  ]);
 }

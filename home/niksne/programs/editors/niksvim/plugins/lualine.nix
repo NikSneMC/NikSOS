b8d11a397951ccd956e01b41ff961fbec1383255
NikSne <commits@niksne.ru>
2024-08-28 11:08:34 +0300

    feat (niksvim): configure lualine

--- home/niksne/programs/editors/niksvim/plugins/lualine.nix
+++ home/niksne/programs/editors/niksvim/plugins/lualine.nix
@@ -3,5 +3,20 @@
     enable = true;
     globalstatus = true;
     theme = "catppuccin";
+    sections = let
+      mkLocationComponent = type: assert (builtins.elem type [ "col" "line" ]); {
+        name = "location";
+        fmt = ''
+          function(text)
+            local current_${type} = vim.fn.${type}('.')
+            local last_${type} = vim.fn.${type}('$')${if type == "col" then " - 1" else ""}
+            return string.format("%d:%d", current_${type}, last_${type})
+          end
+        '';
+      };
+    in {
+      lualine_y = [ (mkLocationComponent "col") ];
+      lualine_z = [ (mkLocationComponent "line") ];
+    };
   };
 }

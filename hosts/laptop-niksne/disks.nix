e67143c708ac463ee5e747b16e3187046e1013f4
NikSne <commits@niksne.ru>
2024-08-30 23:30:31 +0300

    feat (laptop-niksne): update disko config

--- hosts/laptop-niksne/disks.nix
+++ hosts/laptop-niksne/disks.nix
@@ -1,57 +1,59 @@
-{
-  disko.devices = {
-    disk = {
-      sda = {
-        type = "disk";
-        device = "/dev/sda";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              priority = 1;
-              name = "ESP";
-              start = "1M";
-              end = "513M";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-              };
-            };
-            root = {
-              size = "100%";
-              content = {
-                type = "btrfs";
-                extraArgs = [ "-f" ]; 
-                subvolumes = {
-                  "/rootfs" = {
-                    mountpoint = "/";
-                  };
-                  "/home" = {
-                    mountOptions = [ "compress=zstd" ];
-                    mountpoint = "/home";
-                  };
-                  "/nix" = {
-                    mountOptions = [ "compress=zstd" "noatime" ];
-                    mountpoint = "/nix";
-                  };
-                  "/swap" = {
-                    mountpoint = "/.swapvol";
-                    swap = {
-                      swapfile.size = "32G";
-                    };
-                  };
-                };
-
-                mountpoint = "/";
-                swap = {
-                  swapfile = {
-                    size = "32G";
-                  };
-                };
-              };
-            };
+let 
+  mkSubvolumes = subvolumes: builtins.listToAttrs (
+    builtins.map (
+      subvolume: if builtins.isString subvolume then {
+        name = subvolume;
+        value = {
+          mountpoint = subvolume;
+        };
+      } else {
+        name = subvolume.mountpoint;
+        value = subvolume;
+      }
+    )
+    subvolumes
+  );
+in {
+  disko.devices.disk.sda = {
+    type = "disk";
+    device = "/dev/disk/by-id/ata-Samsung_SSD_860_EVO_250GB_S4BFNX0NA04092R";
+    content = {
+      type = "gpt";
+      partitions = {
+        ESP = {
+          priority = 1;
+          name = "ESP";
+          start = "1M";
+          end = "513M";
+          type = "EF00";
+          content = {
+            type = "filesystem";
+            format = "vfat";
+            mountpoint = "/boot";
+          };
+        };
+        root = {
+          size = "100%";
+          content = {
+            type = "btrfs";
+            extraArgs = [ "-f" ]; 
+            subvolumes = mkSubvolumes [
+              "/"
+              {
+                mountOptions = [ "compress=zstd" ];
+                mountpoint = "/home";
+              }
+              {
+                mountOptions = [ "compress=zstd" "noatime" ];
+                mountpoint = "/nix";
+              }
+              {
+                mountpoint = "/.swapvol";
+                swap.swapfile.size = "32G";
+              }
+            ];
+            mountpoint = "/";
+            swap.swapfile.size = "32G";
           };
         };
       };

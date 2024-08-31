8848652dd80b84a395e044cc0af0b5874ac13f5e
NikSne <commits@niksne.ru>
2024-08-31 16:43:38 +0300

    refactor (table-niksne): remove wezterm

--- home/niksne/profiles/table-niksne/default.nix
+++ home/niksne/profiles/table-niksne/default.nix
@@ -7,7 +7,6 @@
     ../../services
 
     ../../terminal/emulators/kitty.nix
-    ../../terminal/emulators/wezterm.nix
   ];
 
   programs.fish.shellAliases.nixos-servers = "cd /etc/nixos/hosts/table-niksne/servers";

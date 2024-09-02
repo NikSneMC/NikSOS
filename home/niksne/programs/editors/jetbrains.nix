6c9496ddf931d1979ec39ccaecdd24d63449148a
NikSne <commits@niksne.ru>
2024-09-02 07:34:27 +0300

    refactor (programs/editors/jetbrains): remove boilerplate && unused IDEs

--- home/niksne/programs/editors/jetbrains.nix
+++ home/niksne/programs/editors/jetbrains.nix
@@ -3,21 +3,22 @@
   npkgs,
   ...
 }: let
-  vmopts = ''
+  patchIDEs = builtins.map (ide: ide.override { vmopts = ''
   --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED
   --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED
 
   -javaagent:${inputs.self.packages.${npkgs.system}.ja-netfilter}/ja-netfilter.jar=jetbrains
-  '';
-  ides = with npkgs.jetbrains; [
-    (idea-ultimate.override { inherit vmopts; })
-    (pycharm-professional.override { inherit vmopts; })
-    (webstorm.override { inherit vmopts; })
-    (rust-rover.override { inherit vmopts; })
-    (goland.override { inherit vmopts; })
-    (phpstorm.override { inherit vmopts; })
-    (clion.override { inherit vmopts; })
-  ];
+  ''; });
+  
+  ides = patchIDEs (with npkgs.jetbrains; [
+    idea-ultimate
+    # pycharm-professional
+    # webstorm
+    # rust-rover
+    # goland
+    # phpstorm
+    # clion
+  ]);
 in {
   programs.jetbrains-remote = {
     enable = true;

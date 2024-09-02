d3edc4bebc3955789db69a2119dc1d97163b32fc
NikSne <commits@niksne.ru>
2024-09-02 07:40:50 +0300

    refactor (system/programs/development): move most stuff to project-specific flakes

--- system/programs/development.nix
+++ system/programs/development.nix
@@ -16,33 +16,9 @@
 
   environment = {
     systemPackages = with pkgs; [
-      zulu8
-      zulu17
-      zulu21
-      
       python312Full
-
-      bun
-      nodejs_22
-      nodePackages.pnpm
-      sass
-      
-      fpc
-
-      gcc
     ];
-    variables = {
-      JAVA_8_HOME = "${pkgs.zulu8}/";
-      JAVA_17_HOME = "${pkgs.zulu17}/";
-      JAVA_21_HOME = "${pkgs.zulu21}/";
-      JAVA_HOME = "${pkgs.zulu21}/";
 
-      PYTHON_PATH = "${pkgs.python312Full}/";
-
-      BUN_PATH = "${pkgs.bun}/";
-      NODE_PATH = "${pkgs.nodejs_22}/";
-
-      GCC_PATH = "${pkgs.libgcc}/";
-    };
+    variables.JAVA_HOME = "${pkgs.zulu21}/";
   };
 }

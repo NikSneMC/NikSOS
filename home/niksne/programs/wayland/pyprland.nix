1f00fafe0a1e637357d7662be750545e873f901d
NikSne <commits@niksne.ru>
2024-09-08 02:00:24 +0300

    refactor (home/programs/wayland): pyprland

--- home/niksne/programs/wayland/pyprland.nix
+++ home/niksne/programs/wayland/pyprland.nix
@@ -1,53 +1,51 @@
 {
-  lib,
   pkgs,
   ...
-}: {
-  home.packages = with pkgs; [
-    pyprland
-  ];
+}: let
+  tomlFormat = pkgs.formats.toml {};
+in {
+  home.packages = with pkgs; [ pyprland ];
 
-  xdg.configFile."hypr/pyprland.toml".text = ''
-    [pyprland]
-    plugins = [
-      "scratchpads",
-      "fetch_client_menu",
-    ]
-
-    [fetch_client_menu]
-    separator = ""
-
-    [scratchpads.term]
-    command = "kitty --class kitty_dropdown"
-    animation = "fromTop"
-    size = "70% 70%"
-    class = "kitty_dropdown"
-    excludes = "*"
-    unfocus = "hide"
-    margin = 75
-    lazy = true
-
-    [scratchpads.volume]
-    command = "pavucontrol"
-    animation = "fromLeft"
-    size = "40% 70%"
-    class = "org.pulseaudio.pavucontrol"
-    excludes = "*"
-    unfocus = "hide"
-    margin = 75
-    lazy = true
-
-    [scratchpads.player]
-    command = "spotify"
-    animation = "fromBottom"
-    size = "70% 70%"
-    match_by = "initialTitle"
-    initialTitle = "Spotify"
-    excludes = "*"
-    unfocus = "hide"
-    margin = 75
-    lazy = true
-  '';
+  xdg.configFile."hypr/pyprland.toml".source = tomlFormat.generate "pyprland-config" {
+    pyprland.plugins = [
+      "scratchpads"
+      "fetch_client_menu"
+    ];
+    fetch_client_menu.separator = "";
+    scratchpads = {
+      term = {
+        command = "kitty --class kitty_dropdown";
+        animation = "fromTop";
+        size = "70% 70%";
+        class = "kitty_dropdown";
+        excludes = "*";
+        unfocus = "hide";
+        margin = 75;
+        lazy = true;
+      };
+      volume = {
+        command = "pavucontrol";
+        animation = "fromLeft";
+        size = "40% 70%";
+        class = "org.pulseaudio.pavucontrol";
+        excludes = "*";
+        unfocus = "hide";
+        margin = 75;
+        lazy = true;
+      };
+      player = {
+        command = "spotify";
+        animation = "fromBottom";
+        size = "70% 70%";
+        match_by = "initialTitle";
+        initialTitle = "Spotify";
+        excludes = "*";
+        unfocus = "hide";
+        margin = 75;
+        lazy = true;
+      };
+    };
+  };
 
   # systemd.user.services.pyprland = {
   #   Unit = {

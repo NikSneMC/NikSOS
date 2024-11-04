f0f88833830ee0d3e536a09d951fc531c4348ed7
NikSne <commits@niksne.ru>
2024-11-05 01:59:12 +0300

    feat (home/hyprland-plugins): add hypr-dynamic-cursors & enable hyprexpo

--- home/niksne/programs/wayland/hyprland/plugins.nix
+++ home/niksne/programs/wayland/hyprland/plugins.nix
@@ -4,10 +4,14 @@
   ...
 }: {
   wayland.windowManager.hyprland = {
-    plugins = with inputs.hyprland-plugins.packages.${pkgs.system}; [
-      # hyprexpo
-      hyprtrails
-    ];
+    plugins = with inputs;
+      (with hyprland-plugins.packages.${pkgs.system}; [
+        hyprexpo
+        hyprtrails
+      ])
+      ++ [
+        hypr-dynamic-cursors.packages.${pkgs.system}.hypr-dynamic-cursors
+      ];
 
     settings.plugin = {
       hyprtrails.color = "$accent";

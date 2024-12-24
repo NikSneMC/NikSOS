096a4496ee902d5667c9db14ab7a85fbb2bd33ac
NikSne <commits@niksne.ru>
2024-12-24 13:53:34 +0300

    chore (flake): update zen-broser

--- home/niksne/programs/browsers/zen.nix
+++ home/niksne/programs/browsers/zen.nix
@@ -4,7 +4,7 @@
   ...
 }: {
   home.packages = [
-    (inputs.zen-browser.packages.${pkgs.system}.beta.specific.override {
+    (inputs.zen-browser.packages.${pkgs.system}.beta.override {
       iconsDir = "${inputs.assets}/icons/zen-browser";
     })
   ];

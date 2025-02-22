a5965753ee9bba2d976e9b62806ce4a1a638062f
NikSne <commits@niksne.ru>
2025-02-22 21:43:45 +0300

    refactor: nh config

--- system/nix/nh.nix
+++ system/nix/nh.nix
@@ -1,7 +1,10 @@
 {
   programs.nh = {
     enable = true;
-    clean.enable = true;
-    clean.extraArgs = "--keep-since 4d --keep 3";
+    flake = "/etc/nixos";
+    clean = {
+      enable = true;
+      extraArgs = "--keep-since 4d --keep 3";
+    };
   };
 }

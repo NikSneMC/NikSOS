2c68de80e1ae93304f84d856a0eac629c92e1ff9
NikSne <commits@niksne.ru>
2025-02-25 08:30:34 +0300

    feat: add garnix cache

--- system/nix/caches.nix
+++ system/nix/caches.nix
@@ -5,5 +5,6 @@
     "hyprland.cachix.org" = "a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=";
     "nix-community.cachix.org" = "mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
     "wezterm.cachix.org" = "kAbhjYUC9qvblTE+s7S+kl5XM1zVa4skO+E/1IDWdH0=";
+    "cache.garnix.io" = "CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=";
   };
 }

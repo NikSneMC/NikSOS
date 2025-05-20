a5e940d6602c10e3761356781e3c086378ffed62
NikSne <commits@niksne.ru>
2025-05-21 01:42:25 +0300

    feat(printing): add some drivers

--- system/hardware/printing.nix
+++ system/hardware/printing.nix
@@ -1,7 +1,13 @@
-{
+{pkgs, ...}: {
   hardware.sane.enable = true;
   services = {
-    printing.enable = true;
+    printing = {
+      enable = true;
+      drivers = with pkgs; [
+        hplipWithPlugin
+        samsung-unified-linux-driver
+      ];
+    };
     avahi = {
       enable = true;
       nssmdns4 = true;

5a96f6a9515227c018fb93a11e24653d7c82317b
NikSne <commits@niksne.ru>
2025-07-30 16:57:16 +0300

    refactor(git): remove aliases
    I want to memorize it all

--- home/terminal/programs/git.nix
+++ home/terminal/programs/git.nix
@@ -16,67 +16,6 @@
       pull.rebase = false;
     };
 
-    aliases = let
-      log = "log --show-notes='*' --abbrev-commit --pretty=format:'%Cred%h %Cgreen(%aD)%Creset -%C(bold red)%d%Creset %s %C(bold blue)<%an>% %Creset' --graph";
-    in {
-      a = "add --patch"; # make it a habit to consciosly add hunks
-      ad = "add";
-
-      b = "branch";
-      ba = "branch -a"; # list remote branches
-      bd = "branch --delete";
-      bdd = "branch -D";
-
-      c = "commit";
-      ca = "commit --amend";
-      cm = "commit --message";
-
-      co = "checkout";
-      cb = "checkout -b";
-      pc = "checkout --patch";
-
-      cl = "clone";
-
-      d = "diff";
-      ds = "diff --staged";
-
-      h = "show";
-      h1 = "show HEAD^";
-      h2 = "show HEAD^^";
-      h3 = "show HEAD^^^";
-      h4 = "show HEAD^^^^";
-      h5 = "show HEAD^^^^^";
-
-      p = "push";
-      pf = "push --force-with-lease";
-
-      pl = "pull";
-
-      l = log;
-      lp = "${log} --patch";
-      la = "${log} --all";
-
-      r = "rebase";
-      ra = "rebase --abort";
-      rc = "rebase --continue";
-      ri = "rebase --interactive";
-
-      rs = "reset";
-      rsh = "reset --hard";
-
-      s = "status --short --branch";
-      ss = "status";
-
-      st = "stash";
-      stc = "stash clear";
-      sth = "stash show --patch";
-      stl = "stash list";
-      stp = "stash pop";
-
-      forgor = "commit --amend --no-edit";
-      oops = "checkout --";
-    };
-
     extraConfig.safe.directory = ["*"];
 
     ignores = [
@@ -99,7 +38,6 @@
     extraConfig.credential.helper = "store";
 
     signing.signByDefault = true;
-
     extraConfig.gpg.format = "ssh";
   };
 }

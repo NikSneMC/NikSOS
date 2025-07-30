ecc75123bf54c2241fb3825390a7302c37321a5f
NikSne <commits@niksne.ru>
2025-07-30 17:09:04 +0300

    feat(wezterm): add some useful keys

--- home/terminal/emulators/wezterm.nix
+++ home/terminal/emulators/wezterm.nix
@@ -40,6 +40,115 @@ in {
         }
         config.window_background_opacity = 0.9
 
+        config.keys = {
+          {
+            key = ",",
+            mods = "CTRL",
+            action = wezterm.action.ActivateTabRelative(-1),
+          },
+          {
+            key = ".",
+            mods = "CTRL",
+            action = wezterm.action.ActivateTabRelative(1),
+          },
+          {
+            key = "<",
+            mods = "CTRL|SHIFT",
+            action = wezterm.action.MoveTabRelative(-1),
+          },
+          {
+            key = ">",
+            mods = "CTRL|SHIFT",
+            action = wezterm.action.MoveTabRelative(1),
+          },
+          {
+            key = "!",
+            mods = "CTRL|SHIFT",
+            action = wezterm.action.MoveTab(0),
+          },
+          {
+            key = "@",
+            mods = "CTRL|SHIFT",
+            action = wezterm.action.MoveTab(1),
+          },
+          {
+            key = "#",
+            mods = "CTRL|SHIFT",
+            action = wezterm.action.MoveTab(2),
+          },
+          {
+            key = "$",
+            mods = "CTRL|SHIFT",
+            action = wezterm.action.MoveTab(3),
+          },
+          {
+            key = "%",
+            mods = "CTRL|SHIFT",
+            action = wezterm.action.MoveTab(4),
+          },
+          {
+            key = "^",
+            mods = "CTRL|SHIFT",
+            action = wezterm.action.MoveTab(5),
+          },
+          {
+            key = "&",
+            mods = "CTRL|SHIFT",
+            action = wezterm.action.MoveTab(6),
+          },
+          {
+            key = "*",
+            mods = "CTRL|SHIFT",
+            action = wezterm.action.MoveTab(7),
+          },
+          {
+            key = "(",
+            mods = "CTRL|SHIFT",
+            action = wezterm.action.MoveTab(8),
+          },
+        }
+
+        for i = 1, 9 do
+          table.insert(config.keys, {
+            key = tostring(i),
+            mods = 'CTRL',
+            action = wezterm.action.ActivateTab(i - 1),
+          })
+        end
+
+
+        disabled_keys = {
+          {
+            key = "PageUp",
+            mods = "CTRL|SHIFT",
+          },
+          {
+            key = "PageDown",
+            mods = "CTRL|SHIFT",
+          },
+          {
+            key = "Tab",
+            mods = "CTRL|SHIFT",
+          },
+          {
+            key = "Tab",
+            mods = "CTRL",
+          },
+          {
+            key = "<",
+            mods = "ALT|SHIFT",
+          },
+          {
+            key = ">",
+            mods = "ALT|SHIFT",
+          },
+        }
+
+        for _, bind in ipairs(disabled_keys) do
+          bind["action"] = wezterm.action.DisableDefaultAssignment
+          table.insert(config.keys, bind)
+        end
+
         return config
       '';
   };

4e97730b411cee8e844051352b665f757aa6b7f5
NikSne <commits@niksne.ru>
2025-09-19 00:43:58 +0300

    feat: sync niksvim theme with the rest of the config

--- home/programs/editors/niksvim.nix
+++ home/programs/editors/niksvim.nix
@@ -1,9 +1,13 @@
 {
+  config,
   inputs,
+  lib,
   pkgs,
   ...
 }: let
-  niksvim = inputs.niksvim.packages.${pkgs.system}.default;
+  niksvim = inputs.niksvim.lib.niksvimConfiguration pkgs {
+    vim.theme.style = lib.mkForce config.theme.flavor;
+  };
 in {
   home.packages = [niksvim];
 }

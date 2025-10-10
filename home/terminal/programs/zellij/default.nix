7500ad8161c66435da534d15764bcea6da48cdb7
NikSne <commits@niksne.ru>
2025-10-10 03:13:40 +0300

    feat(zellij): round corners

--- home/terminal/programs/zellij/default.nix
+++ home/terminal/programs/zellij/default.nix
@@ -12,6 +12,7 @@ args: {
       mirror_session = true;
       show_startup_tips = false;
       show_release_notes = false;
+      ui.pane_frames.rounded_corners = true;
     };
 
     extraConfig =

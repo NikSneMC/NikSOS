20c8c5f8b7b574b0efea745b794b1efc9942ad22
NikSne <commits@niksne.ru>
2025-10-13 05:19:13 +0300

    feat!: drop almost all shell aliases

--- home/terminal/shell/default.nix
+++ home/terminal/shell/default.nix
@@ -5,21 +5,6 @@
   ];
 
   home.shellAliases = {
-    cl = "clear";
-    nfu = "nix flake update";
-    nrt = "nh  os test    -a path:/etc/nixos --";
-    nrs = "nh  os switch  -a path:/etc/nixos --";
-    nrb = "nh  os boot    -a path:/etc/nixos --";
-    nrbu = "nh os boot -u -a path:/etc/nixos --";
-    nrsr = "nrs   && systemctl reboot";
-    nrbr = "nrb   && systemctl reboot";
-    nrbur = "nrsu && systemctl reboot";
-    nrss = "nrs   && systemctl poweroff";
-    nrbs = "nrb   && systemctl poweroff";
-    nrbus = "nrsu && systemctl poweroff";
-    ngc = "sudo nh clean all";
-    lock = "hyprctl dispatch exec hyprlock";
-    unlock = "pkill -10 hyprlock";
     jq = "jaq";
     fetch = "fastfetch";
   };

da90803b876c7a9ea9d02c78b88f579249d7b6ac
NikSne <commits@niksne.ru>
2025-10-26 15:56:11 +0300

    fix(zellij/zsm): configure globally

--- home/terminal/programs/zellij/binds.nix
+++ home/terminal/programs/zellij/binds.nix
@@ -1,4 +1,4 @@
-{config, ...}:
+{...}:
 # kdl
 ''
   keybinds {
@@ -13,16 +13,6 @@
 
       bind "w" {
         LaunchOrFocusPlugin "zsm" {
-          floating true
-          move_to_focused_tab true
-
-          default_layout "custom"
-
-          session_separator "_"
-
-          show_resurrectable_sessions true
-
-          base_paths "${config.home.homeDirectory}/Projects"
         }
         SwitchToMode "Normal"
       }

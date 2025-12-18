28e0fb1dc13ba8e26936888b752afe1b3fad2f95
NikSne <commits@niksne.ru>
2025-12-18 21:00:17 +0300

    feat(system/core/users): provide initial password

--- system/core/users.nix
+++ system/core/users.nix
@@ -2,6 +2,7 @@
   users.users = {
     niksne = {
       isNormalUser = true;
+      initialHashedPassword = "$y$j9T$l4DeIv1F4IavM4cZ662Um1$TLzF2xx.thPu5otqumFf9vpMZKX87.uEol5O0wqHWk5";
       description = "NikSne";
       shell = pkgs.fish;
       extraGroups = [

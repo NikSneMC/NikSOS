d3428d00ccf6ab1a35098516091c18b89dce1163
NikSne <commits@niksne.ru>
2026-02-20 01:28:43 +0300

    chore(nvidia): update to the latest 580 driver

--- system/hardware/nvidia.nix
+++ system/hardware/nvidia.nix
@@ -14,11 +14,11 @@
     nvidiaSettings = true;
     # I have 10xx series GPU so I have to use latest 580 driver
     package = let
-      version = "580.119.02";
+      version = "580.126.18";
     in (config.boot.kernelPackages.nvidiaPackages.stable.overrideAttrs {
       src = pkgs.fetchurl {
         url = "https://download.nvidia.com/XFree86/Linux-x86_64/${version}/NVIDIA-Linux-x86_64-${version}.run";
-        sha256 = "sha256-gCD139PuiK7no4mQ0MPSr+VHUemhcLqerdfqZwE47Nc=";
+        sha256 = "sha256-p3gbLhwtZcZYCRTHbnntRU0ClF34RxHAMwcKCSqatJ0=";
       };
     });
   };

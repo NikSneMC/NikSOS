06339432a8cb495dedf754a87ef7adb7a37840f5
NikSne <commits@niksne.ru>
2026-02-20 23:09:16 +0300

    fix(fastfetch): remove image logo
    It doesn't work anyways + zellij doesn't support images AFAIK

--- home/terminal/programs/fetchers.nix
+++ home/terminal/programs/fetchers.nix
@@ -1,8 +1,4 @@
-{
-  inputs,
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: {
   home.packages = with pkgs; [
     onefetch
     cpufetch
@@ -13,11 +9,6 @@
     enable = true;
     settings = {
       "$schema" = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
-      logo = {
-        type = "kitty";
-        source = "${inputs.assets}/niksos/niksos2048.png";
-        width = 35;
-      };
       display = {
         separator = " │ ";
       };

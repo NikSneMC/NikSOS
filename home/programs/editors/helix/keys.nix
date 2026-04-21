0c4ed848da0af0762bcb549bccc3aa6979265a3f
NikSne <commits@niksne.ru>
2026-04-21 07:21:06 +0300

    fix(helix): revert the file explorer bind
    It is more usable as <space>E with home row mods

--- home/programs/editors/helix/keys.nix
+++ home/programs/editors/helix/keys.nix
@@ -2,13 +2,17 @@
   programs.helix.settings.keys = {
     normal = {
       C-p = ":lsp-workspace-command tinymist.pinMain \"%sh{realpath %{buffer_name}}\"";
-      space.l = [
-        ":new"
-        ":insert-output lazygit"
-        ":buffer-close!"
-        ":redraw"
-        ":reload-all"
-      ];
+      space = {
+        "." = "no_op";
+        E = "file_explorer_in_current_buffer_directory";
+        l = [
+          ":new"
+          ":insert-output lazygit"
+          ":buffer-close!"
+          ":redraw"
+          ":reload-all"
+        ];
+      };
     };
   };
 }
